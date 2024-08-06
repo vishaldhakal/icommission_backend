401de181b83baa22533d4a35c53a744d78049fc5
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-06 10:45:24 +0545

    Dfv

--- affiliate/admin.py
+++ affiliate/admin.py
@@ -2,14 +2,9 @@ from django.contrib import admin
 from .models import Affiliate, Submission
 from unfold.admin import ModelAdmin
 
-@admin.register(Affiliate)
-class AffiliateAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'created_at', 'affiliate_link')
-    search_fields = ('name', 'email')
-    readonly_fields = ('created_at', 'affiliate_link')
-
-    def affiliate_link(self, obj):
-        return obj.create_link()
+class AffiliateAdmin(ModelAdmin):
+      list_display = ('name', 'email', 'created_at', 'id')
+      search_fields = ('name', 'email')
 
 class SubmissionAdmin(ModelAdmin):
       list_display = ('name', 'email', 'phone', 'created_at','message')
