26057ad6fa9342d8b5f08c5054dfd3e3aa0a5572
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-08 17:01:06 +0545

    dfv

--- advance/admin.py
+++ advance/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import CommissionAdvanceRequest
+from unfold.admin import ModelAdmin
 
-admin.site.register(CommissionAdvanceRequest)
+admin.site.register(CommissionAdvanceRequest,ModelAdmin)
