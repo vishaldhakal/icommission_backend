ac9ef7df7e0ce3068df1ee0283e19affd21aa0e6
vishaldhakal <vishaldhakal96@gmail.com>
2024-12-12 00:49:44 +0545

    DfV

--- application/views.py
+++ application/views.py
@@ -173,7 +173,7 @@ def dashboard_analytics(request):
     })
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 100
 
